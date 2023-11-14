931eaa5910b81b2bb45d181a34bb6fd70f1fa348
kingcreek <israel_launion@hotmail.com>
2023-11-14 19:01:06 +0100

    Add files via upload

--- exception_handling2.h
+++ exception_handling2.h
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   exception_handling2.h                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: imurugar <imurugar@student.42madrid.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/13 10:44:55 by imurugar          #+#    #+#             */
-/*   Updated: 2023/11/14 18:53:55 by imurugar         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   exception_handling2.h							  :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: imurugar <imurugar@student.42madrid.com	+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2023/10/13 10:44:55 by imurugar		  #+#	#+#			 */
+/*   Updated: 2023/11/14 18:53:55 by imurugar		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #ifndef EXCEPTION_HANDLING_H
@@ -56,7 +56,7 @@ const char *_exception_string[] = {
 
 typedef struct
 {
-    jmp_buf _catch_jmp_buf;
+	jmp_buf _catch_jmp_buf;
 	int _catch_value;
 	volatile bool _retry_attempt;
 	volatile bool _init_signals;
@@ -75,93 +75,93 @@ _Thread_local volatile bool _init_signals = false;
 
 void _catch_sig_handle(int sig);
 
-#define _trace_prepare(X)                          \
-	do                                             \
-	{                                              \
-		_ex_ctx[_ex_i]._trextrigger = true;        \
-		_ex_ctx[_ex_i]._trexfile = __FILE__;       \
-		_ex_ctx[_ex_i]._trexfunc = __func__;       \
-		_ex_ctx[_ex_i]._trexline = __LINE__ + 0UL; \
-		_ex_ctx[_ex_i]._trex = X;                  \
+#define _trace_prepare(X)							\
+	do												\
+	{												\
+		_ex_ctx[_ex_i]._trextrigger = true;			\
+		_ex_ctx[_ex_i]._trexfile = __FILE__;		\
+		_ex_ctx[_ex_i]._trexfunc = __func__;		\
+		_ex_ctx[_ex_i]._trexline = __LINE__ + 0UL;	\
+		_ex_ctx[_ex_i]._trex = X;					\
 	} while (false)
 
 
-#define _trace_throw(X)                       \
-	fprintf(stderr,                           \
-			"[%s] %s:%lu: Exception %s.\n",   \
-			_ex_ctx[_ex_i]._trexfile,         \
-			_ex_ctx[_ex_i]._trexfunc,         \
-			_ex_ctx[_ex_i]._trexline,         \
+#define _trace_throw(X)						\
+	fprintf(stderr,							\
+			"[%s] %s:%lu: Exception %s.\n",	\
+			_ex_ctx[_ex_i]._trexfile,		\
+			_ex_ctx[_ex_i]._trexfunc,		\
+			_ex_ctx[_ex_i]._trexline,		\
 			_exception_string[_ex_ctx[_ex_i]._trex - EX_BEGIN])
 
-#define _trace_signal()                     \
-	fprintf(stderr,                         \
-			"Interrupted by signal: %s.\n", \
+#define _trace_signal()												\
+	fprintf(stderr,													\
+			"Interrupted by signal: %s.\n",							\
 			_exception_string[_ex_ctx[_ex_i]._trex - EX_BEGIN])
 
-#define _trace_if_triggered()     					  \
-	do                            					  \
-	{                             					  \
-		if (_ex_ctx[_ex_i]._trextrigger)         	  \
-			if (_ex_ctx[_ex_i]._trex >= SIG_BEGIN)	  \
-				_trace_signal();  					  \
-			else                  					  \
-				_trace_throw();    					  \
-		_ex_ctx[_ex_i]._trextrigger = false;  	      \
+#define _trace_if_triggered()	 					\
+	do												\
+	{							 					\
+		if (_ex_ctx[_ex_i]._trextrigger)		 	\
+			if (_ex_ctx[_ex_i]._trex >= SIG_BEGIN)	\
+				_trace_signal();  					\
+			else				  					\
+				_trace_throw();						\
+		_ex_ctx[_ex_i]._trextrigger = false;  		\
 	} while (false)
 
-#define print_error()               \
+#define print_error()			\
 	_trace_if_triggered();
 	
-#define _bind_signals()                       \
-	do                                        \
-	{                                         \
-		for (uint8_t i = 1; i < SIG_MAX; i++) \
-			signal(i, _catch_sig_handle);     \
-		_init_signals = true;                 \
+#define _bind_signals()					 		\
+	do											\
+	{											\
+		for (uint8_t i = 1; i < SIG_MAX; i++)	\
+			signal(i, _catch_sig_handle);		\
+		_init_signals = true;					\
 	} while (false)
 
-#define try                             								    \
-	if (!_init_signals)                 							   	    \
-		_bind_signals();               									    \
-	_ex_i++;                              									\
-	_ex_ctx[_ex_i].catched = false;        									\
+#define try							 										\
+	if (!_init_signals)				 							   			\
+		_bind_signals();			   										\
+	_ex_i++;							  									\
+	_ex_ctx[_ex_i].catched = false;											\
 	_ex_ctx[_ex_i]._catch_value = setjmp(_ex_ctx[_ex_i]._catch_jmp_buf); 	\
-	if (_ex_ctx[_ex_i]._retry_attempt)                    					\
-	{                                      									\
-		_ex_ctx[_ex_i]._catch_value = 0;                 					\
-		_ex_ctx[_ex_i]._retry_attempt = false;            					\
-		_ex_ctx[_ex_i].catched = false;        								\
-	}                                      									\
+	if (_ex_ctx[_ex_i]._retry_attempt)										\
+	{									  									\
+		_ex_ctx[_ex_i]._catch_value = 0;				 					\
+		_ex_ctx[_ex_i]._retry_attempt = false;								\
+		_ex_ctx[_ex_i].catched = false;										\
+	}									  									\
 	if (!_ex_ctx[_ex_i]._catch_value && _ex_ctx[_ex_i].catched == false)
 
-#define catch(X)                               										\
+#define catch(X)							   										\
 	else if (_ex_ctx[_ex_i]._catch_value == X && _ex_ctx[_ex_i].catched == true) 	\
 		_ex_i--; 																	\
 
 #define catch_all  										\
-	else if (_ex_ctx[_ex_i].catched == true)            \
+	else if (_ex_ctx[_ex_i].catched == true)			\
 		_ex_i--; 										\
 
 #define finally
 
-#define throw(X)                        				 \
-	do                                 	  				 \
-	{                                    				 \
-		_ex_ctx[_ex_i].catched = true;   				 \
-		_trace_prepare(X);               				 \
-		longjmp(_ex_ctx[_ex_i]._catch_jmp_buf, X);       \
+#define throw(X)										\
+	do								 	  				\
+	{													\
+		_ex_ctx[_ex_i].catched = true;   				\
+		_trace_prepare(X);			   				 	\
+		longjmp(_ex_ctx[_ex_i]._catch_jmp_buf, X);	 	\
 	} while (false)
 
-#define retry()                          					\
-	do                                  			 		\
-	{                          								\
-		if (_ex_ctx[_ex_i + 1]._catch_value != 0)           \
-		{      												\
-			_ex_i++; 										\
-			_ex_ctx[_ex_i]._retry_attempt = true;           \
-			longjmp(_ex_ctx[_ex_i]._catch_jmp_buf, 1);      \
-		}   												\
+#define retry()						  					\
+	do								  			 		\
+	{						  							\
+		if (_ex_ctx[_ex_i + 1]._catch_value != 0)		\
+		{	  											\
+			_ex_i++; 									\
+			_ex_ctx[_ex_i]._retry_attempt = true;		\
+			longjmp(_ex_ctx[_ex_i]._catch_jmp_buf, 1);	\
+		}   											\
 	} while (false)
 
 void _catch_sig_handle(int sig)
